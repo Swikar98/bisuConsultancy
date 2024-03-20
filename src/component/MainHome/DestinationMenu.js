1afe0b60c156f03a4aae31ede10bd90b1dcecc67
Swikar98 <swikarsingh123@gmail.com>
2024-03-20 13:03:23 +0545

    animation try

--- src/component/MainHome/DestinationMenu.js
+++ src/component/MainHome/DestinationMenu.js
@@ -4,8 +4,8 @@ import MenuItem from "./services.Item"
 import SectionHead from "./SectionHead"
 export default function DestinationMenu(){
     return(
-        <section className=" bg-white m-4 rounded-lg mt-10" >
-            <div className="text-center mt-10 p-8 ">
+        <section className=" bg-white m-4  rounded-lg mt-10" >
+            <div className="text-center animate-typing overflow-hidden whitespace-nowrap mt-10 p-8 ">
             <SectionHead
             subHeader={'Your scholastic objectives are waiting to be fulfilled.'}
             mainHeader={'Destination'}/>
--- src/component/MainHome/ServicesMenu.js
+++ src/component/MainHome/ServicesMenu.js
@@ -3,8 +3,8 @@ import ServicesItem from "./services.Item"
 import SectionHead from "./SectionHead"
 export default function ServicesMenu(){
     return(
-        <section className="bg-white" >
-            <div className="text-center mt-10 p-8">
+        <section className="bg-white " >
+            <div className="text-center animate-typing overflow-hidden whitespace-nowrap  mt-10 p-8">
             <SectionHead
             subHeader={'most popular services '}
             mainHeader={'Our Services'}/>
