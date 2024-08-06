31057bd032f901d2b7f966fe4c067a9949d26ba9
Swikar98 <swikarsingh123@gmail.com>
2024-08-06 23:39:00 +0545

    all update

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -11,9 +11,8 @@ export default function Home() {
   return (
     <>
       <Slide/>
-      <HeroSection/>
       <Choose/>
-    
+      <HeroSection/>
       <ServicesMenu/>
     </>
   );
--- src/component/MainHome/hero/herosection.jsx
+++ src/component/MainHome/hero/herosection.jsx
@@ -1,20 +1,25 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
-import anime from 'animejs';
+import anime from "animejs";
 import { PiCertificate } from "react-icons/pi";
-import { FaUserGraduate, FaUserFriends, FaPassport, FaPlane, FaBriefcase, FaClipboardList } from "react-icons/fa";
+import {
+  FaUserGraduate,
+  FaUserFriends,
+  FaClipboardList,
+  FaPlane,
+  FaBriefcase,
+} from "react-icons/fa";
 
 const HeroSection = () => {
   const visaTypes = [
-    { name: "Student Visa", icon: FaUserGraduate, color: "text-red-600" },
-    { name: "Partner Visa", icon: FaUserFriends, color: "text-green-600" },
-    { name: "Training Visa", icon: FaClipboardList, color: "text-blue-600" },
-    { name: "Visitor Visa", icon: FaPlane, color: "text-yellow-600" },
-    { name: "Graduate Visa", icon: PiCertificate, color: "text-purple-600" },
-    { name: "Skilled Visa", icon: FaBriefcase, color: "text-orange-600" },
+    { name: "Student Visa", icon: FaUserGraduate, color: "bg-red-600" },
+    { name: "Partner Visa", icon: FaUserFriends, color: "bg-green-600" },
+    { name: "Training Visa", icon: FaClipboardList, color: "bg-blue-600" },
+    { name: "Visitor Visa", icon: FaPlane, color: "bg-yellow-600" },
+    { name: "Graduate Visa", icon: PiCertificate, color: "bg-purple-600" },
+    { name: "Skilled Visa", icon: FaBriefcase, color: "bg-orange-600" },
   ];
 
-  // Framer Motion scroll and transform hooks
   const { scrollY } = useScroll();
   const yTransform = useTransform(scrollY, [0, 500], [0, 50]);
 
@@ -22,18 +27,19 @@ const HeroSection = () => {
 
   useEffect(() => {
     anime({
-      targets: '.background-element',
-      translateX: () => anime.random(-20, 50),
+      targets: ".background-element",
+      translateX: () => anime.random(-10, 30),
       translateY: () => anime.random(-50, 50),
       duration: 2000,
-      easing: 'easeInOutQuad',
-      direction: 'alternate',
-      loop: true
+      easing: "easeInOutQuad",
+      direction: "alternate",
+      loop: true,
     });
   }, []);
 
   return (
-    <div className="relative bg-gradient-to-r from-blue-300 via-blue-400 to-blue-500 h-screen lg:h-[60vh] flex items-center justify-center overflow-hidden">
+    <div className="relative bg-gradient-to-r from-blue-300 via-blue-400 to-blue-500 h-screen lg:h-[60vh]
+     flex items-center justify-center overflow-hidden">
       <div ref={backgroundRef} className="absolute inset-0"></div>
       {[...Array(20)].map((_, i) => (
         <div
@@ -41,32 +47,40 @@ const HeroSection = () => {
           className="background-element w-10 h-10 bg-white opacity-20 absolute rounded-full"
           style={{
             top: `${Math.random() * 100}%`,
-            left: `${Math.random() * 100}%`
+            left: `${Math.random() * 100}%`,
           }}
         ></div>
       ))}
-      <div className="relative z-10 p-4 container bg-opacity-80 rounded-lg shadow-lg">
-        <div className="flex items-center  justify-center lg:h-[50vh] h-full sm:h-full p-4">
-          <div className="px-2 container sm:px-2 ">
-            <section className="p-2 rounded-xl">
-              <div className="text-center mb-2">
-                <h2 className="text-xl font-bold">Do You Want To Complete Your Degree Fast Through the RPL</h2>
-                <h3 className="text-lg">We Can Shape Your Vision.</h3>
+      <div className="relative z-10 p-2 container bg-white bg-opacity-20 rounded-lg shadow-lg">
+        <div className="flex items-center justify-center lg:h-[50vh] h-[90vh] p-4">
+          <div className="container ">
+            <section className=" rounded-xl">
+              <div className="mb-1">
+                <h2 className="text-xl font-bold text-center">
+                  Do You Want To Complete Your Degree Fast Through the RPL
+                </h2>
+                <h3 className="text-lg text-center">
+                  We Can Shape Your Vision.
+                </h3>
               </div>
-              <div className="grid grid-cols-2 container lg:grid-cols-6 md:grid-cols-3 gap-2">
+              <div className="grid grid-cols-2 lg:grid-cols-6 md:grid-cols-3 gap-4">
                 {visaTypes.map((visaType, index) => (
                   <motion.div
                     key={index}
-                    className="hover:shadow-md  hover:shadow-black/25 transition-all border-2 rounded-lg"
-                    style={{ y: yTransform }} // Apply scroll-based transform
+                    className="hover:shadow-md hover:shadow-black/25 transition-all border-2 rounded-lg p-3"
+                    style={{ y: yTransform }} 
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.5, delay: index * 0.2 }}
                   >
-                    <div className="text-center flex justify-center items-center p-2 ">
-                      <visaType.icon className={`${visaType.color} text-xl sm:text-2xl lg:text-3xl block mx-auto`} />
+                    <div
+                      className={`text-center flex justify-center items-center p-3 border-2 rounded-full ${visaType.color} bg-opacity-50`}
+                    >
+                      <visaType.icon className="text-white text-xl sm:text-2xl lg:text-3xl block " />
                     </div>
-                    <h4 className="lg:font-semibold p-4 text-center ">{visaType.name}</h4>
+                    <h4 className="font-semibold py-2 text-center">
+                      {visaType.name}
+                    </h4>
                   </motion.div>
                 ))}
               </div>
